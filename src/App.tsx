36a7fa876275af8ba9ea3aab17649c6a04f0a6e2
srilatha2012 <srilathp2012@gmail.com>
2026-04-27 14:57:36 -0400

    Fix src folder tracking

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+    <h1>Task Manager</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/TaskItem/TaskItem.tsx
@@ -0,0 +1,23 @@
+import type { TaskItemProps, TaskStatus } from "../../types"
+
+export function TaskItem({ task, onStatusChange, onDelete }: TaskItemProps) {
+
+    return (
+        <div>
+            <h2>{task.title}</h2>
+            <p>{task.description}</p>
+            <p>Status: {task.status}</p>
+            <p>Priority: {task.priority}</p>
+            <p>Due: {task.dueDate}</p>
+
+            <select
+                value={task.status}
+                onChange={(e) => onStatusChange(task.id, e.target.value as TaskStatus)}>
+                <option value="Pending">Pending</option>
+                <option value="In-Progress">In Progress</option>
+                <option value="completed">Completed</option>
+            </select>
+            <button onClick={() => onDelete(task.id)}>Delete</button>
+        </div>
+    )
+}
