eb1c597f3893501c5750c5d6d22e91729f4626d8
srilatha2012 <srilathp2012@gmail.com>
2026-04-27 15:38:46 -0400

    Added TaskFilter component

---
+++ src/components/TaskFilter/TaskFilter.tsx
@@ -0,0 +1,38 @@
+import type { TaskFilterProps, TaskStatus } from "../../types"
+
+export function TaskFilter({ onFilterChange }: TaskFilterProps) {
+
+    return (
+        <div>
+
+            <select
+                onChange={(e) =>
+                    onFilterChange({
+                        status: e.target.value === "" ? undefined : (e.target.value as TaskStatus),
+                    })
+                }>
+                <option value="">All Status</option>
+                <option value="pending">Pending</option>
+                <option value="in-progress">In Progress</option>
+                <option value="completed">Completed</option>
+            </select>
+            <select
+                onChange={(e) =>
+                    onFilterChange({
+                        priority:
+                            e.target.value === ""
+                                ? undefined
+                                : (e.target.value as "low" | "medium" | "high")
+                    })
+
+                } >
+                <option value="">All Priority</option>
+                <option value="pending">Low</option>
+                <option value="in-progress">Medium</option>
+                <option value="completed">High</option>
+
+            </select>
+
+        </div>
+    )
+}
